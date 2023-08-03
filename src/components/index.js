4cdcbf7057487fa0e3e6e20f9b474e6e2731cc9f
Alexandre Lopes <alexandre9999lopes@gmail.com>
2023-08-03 14:13:33 -0300

    feat: add datefield and form session component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import FormButton from '@/components/FormButton/FormButton.vue'
 import AnsweredField from '@/components/AnsweredField/AnsweredField.vue'
 import CheckboxField from '@/components/CheckboxField/CheckboxField.vue'
+import DateField from '@/components/DateField/DateField.vue'
+import FormSession from '@/components/FormSession/FormSession.vue'
 
 export default {
   FormButton,
   AnsweredField,
   CheckboxField,
+  DateField,
+  FormSession,
 }
