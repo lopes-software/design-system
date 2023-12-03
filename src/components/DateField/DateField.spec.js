0bd671e5e9c11aa8023be105f359af4c1c26b50d
Alexandre Lopes <alexandre9999lopes@gmail.com>
2023-12-03 11:10:00 -0300

    feat: update date picker package

--- src/components/DateField/DateField.spec.js
+++ src/components/DateField/DateField.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import DateField from '@/components/DateField/DateField.vue'
 
-jest.mock('vue2-datepicker/index.css', () => '')
+jest.mock('vue-datepicker-next/index.css', () => '')
 
 describe('DateField', () => {
   const label = 'Start at ?'
