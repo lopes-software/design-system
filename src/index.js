f3a48b6bb4a187bb6ab82b5beba659d6c3a6bfc2
Alexandre Lopes <alexandre9999lopes@gmail.com>
2024-02-13 20:12:27 -0300

    test

--- src/index.js
+++ src/index.js
@@ -39,5 +39,17 @@ export default {
   }
 }
 
-export { default as DsInputField } from '@/components/InputField/InputField.vue'
-export { default as DsFormButton } from '@/components/FormButton/FormButton.vue'
+export {default as DsFormButton } from '@/components/FormButton/FormButton.vue'
+export {default as DsAnsweredField } from '@/components/AnsweredField/AnsweredField.vue'
+export {default as DsCheckboxField } from '@/components/CheckboxField/CheckboxField.vue'
+export {default as DsDateField } from '@/components/DateField/DateField.vue'
+export {default as DsFormSession } from '@/components/FormSession/FormSession.vue'
+export {default as DsFormTag } from '@/components/FormTag/FormTag.vue'
+export {default as DsInputField } from '@/components/InputField/InputField.vue'
+export {default as DsSelectField } from '@/components/SelectField/SelectField.vue'
+export {default as DsTagSelector } from '@/components/TagSelector/TagSelector.vue'
+export {default as DsTextAreaField } from '@/components/TextAreaField/TextAreaField.vue'
+export {default as DsModalDialog } from '@/components/ModalDialog/ModalDialog.vue'
+export {default as DsListPagination } from '@/components/ListPagination/ListPagination.vue'
+export {default as DsTableItems } from '@/components/TableItems/TableItems.vue'
+export {default as DsMultiCheckboxField } from '@/components/MultiCheckboxField/MultiCheckboxField.vue'
