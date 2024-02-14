0dda906c153121b240f4dcf617432dccb1b361fa
Alexandre Lopes <alexandre9999lopes@gmail.com>
2024-02-13 21:16:37 -0300

    t

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+import { default as DsFormButton } from '@/components/FormButton/FormButton.vue'
+import { default as DsAnsweredField } from '@/components/AnsweredField/AnsweredField.vue'
+import { default as DsCheckboxField } from '@/components/CheckboxField/CheckboxField.vue'
+import { default as DsDateField } from '@/components/DateField/DateField.vue'
+import { default as DsFormSession } from '@/components/FormSession/FormSession.vue'
+import { default as DsFormTag } from '@/components/FormTag/FormTag.vue'
+import { default as DsInputField } from '@/components/InputField/InputField.vue'
+import { default as DsSelectField } from '@/components/SelectField/SelectField.vue'
+import { default as DsTagSelector } from '@/components/TagSelector/TagSelector.vue'
+import { default as DsTextAreaField } from '@/components/TextAreaField/TextAreaField.vue'
+import { default as DsModalDialog } from '@/components/ModalDialog/ModalDialog.vue'
+import { default as DsListPagination } from '@/components/ListPagination/ListPagination.vue'
+import { default as DsTableItems } from '@/components/TableItems/TableItems.vue'
+import { default as DsMultiCheckboxField } from '@/components/MultiCheckboxField/MultiCheckboxField.vue'
+
+const components = {
+  DsFormButton,
+  DsAnsweredField,
+  DsCheckboxField,
+  DsDateField,
+  DsFormSession,
+  DsFormTag,
+  DsInputField,
+  DsSelectField,
+  DsTagSelector,
+  DsTextAreaField,
+  DsModalDialog,
+  DsListPagination,
+  DsTableItems,
+  DsMultiCheckboxField,
+}
+
 export default {
   install: (app) => {
     Object.keys(components).forEach(name => {
@@ -6,17 +38,19 @@ export default {
   }
 }
 
-export { default as DsFormButton } from '@/components/FormButton/FormButton.vue'
-export { default as DsAnsweredField } from '@/components/AnsweredField/AnsweredField.vue'
-export { default as DsCheckboxField } from '@/components/CheckboxField/CheckboxField.vue'
-export { default as DsDateField } from '@/components/DateField/DateField.vue'
-export { default as DsFormSession } from '@/components/FormSession/FormSession.vue'
-export { default as DsFormTag } from '@/components/FormTag/FormTag.vue'
-export { default as DsInputField } from '@/components/InputField/InputField.vue'
-export { default as DsSelectField } from '@/components/SelectField/SelectField.vue'
-export { default as DsTagSelector } from '@/components/TagSelector/TagSelector.vue'
-export { default as DsTextAreaField } from '@/components/TextAreaField/TextAreaField.vue'
-export { default as DsModalDialog } from '@/components/ModalDialog/ModalDialog.vue'
-export { default as DsListPagination } from '@/components/ListPagination/ListPagination.vue'
-export { default as DsTableItems } from '@/components/TableItems/TableItems.vue'
-export { default as DsMultiCheckboxField } from '@/components/MultiCheckboxField/MultiCheckboxField.vue'
+export {
+  DsFormButton,
+  DsAnsweredField,
+  DsCheckboxField,
+  DsDateField,
+  DsFormSession,
+  DsFormTag,
+  DsInputField,
+  DsSelectField,
+  DsTagSelector,
+  DsTextAreaField,
+  DsModalDialog,
+  DsListPagination,
+  DsTableItems,
+  DsMultiCheckboxField,
+}
